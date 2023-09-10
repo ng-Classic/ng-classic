293ccf43c8bb65e113b657d9208608a457031ead
Jordan-Hall <Jordan-Hall@users.noreply.github.com>
2023-09-10 00:45:22 +0000

    Update Nx Angular

--- angular-classic/packages/nx/nx-angular-classic/src/migrations/update-15-5-0/update-angular-cli.ts
+++ angular-classic/packages/nx/nx-angular-classic/src/migrations/update-15-5-0/update-angular-cli.ts
@@ -6,11 +6,11 @@ export default async function (tree: Tree) {
   let shouldFormat = false;
 
   updateJson(tree, 'package.json', (json) => {
-    if (json.devDependencies?.['@angular/cli']) {
-      json.devDependencies['@angular/cli'] = angularCliVersion;
+    if (json.devDependencies?.['@angular-classic/cli']) {
+      json.devDependencies['@angular-classic/cli'] = angularCliVersion;
       shouldFormat = true;
-    } else if (json.dependencies?.['@angular/cli']) {
-      json.dependencies['@angular/cli'] = angularCliVersion;
+    } else if (json.dependencies?.['@angular-classic/cli']) {
+      json.dependencies['@angular-classic/cli'] = angularCliVersion;
       shouldFormat = true;
     }
 
